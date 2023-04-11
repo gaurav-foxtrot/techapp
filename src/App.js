9a28053030f4eeacee474a330742bed0a3f5f7c6
gauravkamble <gauravkamble2310@gmail.com>
2023-04-11 12:44:38 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
 
-import { useEffect } from 'react';
+import { useEffect,useState } from 'react';
 import './App.css';
 import HomePage from './Pages/HomePage';
 
 
 function App() {
   
+  const[data,setData] = useState([{}])
+
   useEffect(()=>{
-    
+    fetch("/converted_code").then(
+      res => res.json()
+    ).then(
+      data => {
+        setData(data)
+        console.log(data)
+      }
+    )
   },[])
 
-const convert = async() => {
+const convert = async(e) => {
+  e.preventDefault();
+  const res = await fetch("/InputLang",{
+    method:"POST",
+    headers:{
+      "Content-Type" : "application/json",
+    },
+  
+  })
+
+  
+   
 
 }
+
+
 
 
   return (
@@ -20,7 +42,7 @@ const convert = async() => {
       <h1 className=' bg-slate-900 text-4xl text-center text-orange-600 mt-5' >Here's your CODE CONVERTER</h1>
       <HomePage/>
        <button onClick={convert} class="bg-transparent hover:bg-white text-orange-600 font-semibold hover:text-orange-600 py-2 px-4 border border-white hover:border-transparent rounded mt-96 ml-10">
-  Convert
+        Convert
 </button>
     </div>
   );
